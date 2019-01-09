743b4c4b352648a558ef8655cadf236db56d5121
Trine <trinem95@hotmail.com>
2019-01-09 12:56:29 +0100

    connected webpage

--- 3.nettavisen/main.js
+++ 3.nettavisen/main.js
@@ -1,4 +1,23 @@
+// HTML-elementer
 const skjema = document.querySelector("#skjema");
 const inpOverskrift = document.querySelector("#inpOverskrift");
 const inpHovedtekst = document.querySelector("#inpHovedtekst");
 const inpBilde = document.querySelector("#inpBilde");
+
+// DATABASE-kobling
+const db = firebase.database();
+
+// Angi hvor i databasen vi skal lagre nettsiden
+const nettsider = db.ref("nettsider");
+
+function lagreNettside(evt) {
+    evt.preventDefault();
+    nettsider.push({
+        overskrift: inpOverskrift.value,
+        hovedtekst: inpHovedtekst.value,
+        bilde: inpBilde.value
+    })
+}
+
+// Eventlistenere
+skjema.addEventListener("submit", lagreNettside);
