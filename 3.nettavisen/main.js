65c4f228a816b447a4e992a26f2e432daafa1574
Trine <trinem95@hotmail.com>
2019-01-09 12:47:24 +0100

    nettavis

---
+++ 3.nettavisen/main.js
@@ -0,0 +1,4 @@
+const skjema = document.querySelector("#skjema");
+const inpOverskrift = document.querySelector("#inpOverskrift");
+const inpHovedtekst = document.querySelector("#inpHovedtekst");
+const inpBilde = document.querySelector("#inpBilde");
